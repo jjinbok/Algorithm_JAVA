e69c65e0a1f20e9000ceb58201d44d60f28d904e
jjinbok <haba8730@naver.com>
2024-12-27 00:02:45 +0800

    boj9625

---
+++ src/algorithm/baekjoon/sliver/boj9625.java
@@ -0,0 +1,34 @@
+package algorithm.baekjoon.sliver;
+
+import java.util.Scanner;
+
+public class boj9625 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int k = scanner.nextInt();
+
+        int [] arrayA = new int[1000];
+        int [] arrayB = new int[1000];
+
+        arrayA[0] = 1;
+        arrayB[0] = 0; // 이건 누르지 않은 상태
+
+        arrayA[1] = 0; // 이게 1번 누른 거
+        arrayB[1] = 1;
+
+        arrayA[2] = 1;
+        arrayB[2] = 1;
+
+        for (int i = 3; i < k + 1; i++) {
+            arrayA[i] = arrayA[i - 1];
+            arrayB[i] = arrayB[i - 1];
+
+            arrayA[i] -= arrayA[i - 1]; // A의 개수 n만큼 줄이고 A 0 B 1
+            arrayB[i] += arrayA[i - 1]; // B의 개수 n만큼 증가 A 0 B 2
+            arrayA[i] += arrayB[i - 1]; // A의 개수 A 1 B 2
+        }
+
+        System.out.print(arrayA[k] + " " + arrayB[k]);
+    }
+}
