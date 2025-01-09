e33af5f0ed7e57c249139044fad3b8ff2b314a81
jjinbok <haba8730@naver.com>
2025-01-10 00:48:20 +0900

    boj11022

---
+++ src/algorithm/baekjoon/bronze/boj11022.java
@@ -0,0 +1,29 @@
+package algorithm.baekjoon.bronze;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class boj11022 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i < n + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #" + i + ": " + a + " + " + b + " = " + (a+b) + "\n");
+        }
+
+        bw.close();
+    }
+
+}
