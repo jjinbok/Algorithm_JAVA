0c2c175200a87be1ab65817429465550c885c566
jjinbok <haba8730@naver.com>
2025-06-08 22:44:22 +0900

    boj10988

---
+++ src/algorithm/baekjoon/bronze/boj10988.java
@@ -0,0 +1,34 @@
+package algorithm.baekjoon.bronze;
+
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        boolean palindrome = true;
+
+        String word = br.readLine();
+        Character[] right = new Character[word.length()];
+
+        for (int i = 0; i < word.length(); i++) {
+            Character c = word.charAt(i);
+            right[i] = c;
+        }
+
+        for (int i = 0; i < (word.length() / 2); i++) {
+            if (!right[i].equals(right[(word.length() - 1) - i])) {
+                palindrome = false;
+            }
+        }
+
+        if (palindrome) {
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.close();
+    }
+}
